1cef4b44db0af4b167ff1be160d6493c1b2ab42e
VIKTORIIA HOLOVKO <golovkovictoria@gmail.com>
2023-09-24 22:38:10 +0200

    adding react router

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,9 @@ import { SideNav } from "./components/SideNav";
 // import { db } from "./utils/firebase";
 // import { onValue, ref } from "firebase/database";
 import { RECIPES } from "./constants/recipes";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-
-  
-  
   // useEffect(() => {
   //   const fetchData = () => {
   //     const testRef = ref(db, 'recipes');
@@ -31,8 +29,32 @@ function App() {
           <div>Impressum</div>
         </SidePanel>
         <ContentPanel>
-         {RECIPES.map((recipe) => <Card key={recipe.id}><RecipeCard /></Card>)}
-         {/* <h3>You have no recipes yet</h3> */}
+          <Router>
+            <Routes>
+              <Route
+                path="/list"
+                element={<div>This is your shopping list</div>}
+              />
+              <Route
+                path="/planner"
+                element={<div>This is your food planner</div>}
+              />
+              <Route
+                path="/"
+                element={
+                  <div>
+                    {RECIPES.map((recipe) => (
+                      <Card key={recipe.id}>
+                        <RecipeCard />
+                      </Card>
+                    ))}
+                  </div>
+                }
+              />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </Router>
+          {/* <h3>You have no recipes yet</h3> */}
         </ContentPanel>
       </Layout>
     </div>
