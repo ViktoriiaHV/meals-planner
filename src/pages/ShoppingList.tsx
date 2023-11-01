3501c1d30200e8a6d6fe22c621d2e9d4f1a5c403
VIKTORIIA HOLOVKO <golovkovictoria@gmail.com>
2023-11-01 19:47:29 +0100

    add item

--- src/pages/ShoppingList.tsx
+++ src/pages/ShoppingList.tsx
@@ -1,17 +1,21 @@
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Checkbox } from "../components/Checkbox";
-import RootState from "../store";
-import { checkItem, removeItem, renameItem } from "../store/shoppingListSlice/shoppingListSlice";
+import type RootState from "../store";
+import {
+  checkItem,
+  removeItem,
+  addItem,
+} from "../store/shoppingListSlice/shoppingListSlice";
 
 export function ShoppingList() {
-  const shoppingList = useSelector((state: RootState) => state.shoppingList.items);
-  const dispatch = useDispatch()
+  const shoppingList = useSelector(
+    (state: RootState) => state.shoppingList.items
+  );
+  const dispatch = useDispatch();
 
-  function handleOnCheck() {
-
-  }
-  function handleInputChange(id: any, newValue: string) {
-    dispatch(renameItem(({id, newValue})))
+  function handleInputChange(value: string) {
+    dispatch(addItem(value));
   }
 
   return (
@@ -29,13 +33,52 @@ export function ShoppingList() {
                   checked={item.checked}
                   onChange={() => dispatch(checkItem(item.id))}
                 />
-                <input type='text' readOnly className="shopping__input" value={item.title} onChange={(e) => handleInputChange(item.id, e.target.value)} />
-                <button className="btn btn--small" onClick={() => dispatch(removeItem(item.id))}>X</button>
+                <input
+                  type="text"
+                  readOnly
+                  className="shopping__input"
+                  value={item.title}
+                />
+                <button
+                  className="btn btn--small"
+                  onClick={() => dispatch(removeItem(item.id))}
+                >
+                  X
+                </button>
               </li>
             );
           })}
+          <NewItem onNewItem={handleInputChange} />
         </ul>
       </div>
     </div>
+  );
+}
+
+function NewItem({ onNewItem }: { onNewItem: (value: string) => void }) {
+  const [value, setValue] = useState<string>("");
+
+  const addNewItem = () => {
+    if (value.trim().length < 1) return;
+    onNewItem(value);
+    setValue("");
+  };
+
+  return (
+    <>
+      <input
+        autoFocus
+        style={{ marginLeft: "2.1rem", marginRight: '2px' }}
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="shopping__input"
+        onBlur={addNewItem}
+        onKeyDown={(e) => (e.key === "Enter" ? addNewItem() : null)}
+      />
+      <button className="btn btn--small" onClick={addNewItem}>
+        +
+      </button>
+    </>
   );
 }
